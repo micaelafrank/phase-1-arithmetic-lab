52c856a90fd871f39151b2d5c518732618621b65
Micaela Frank <micaelalfrank@gmail.com>
2022-04-26 16:28:39 -0400

    commit tree 9ffc88619a1ed6e7e7dd72efabb8e67db7441a76

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 1000) + 1;
+
+const num3 = 39;
+const num4 = 5;
+const mod = num3%num4;
+
+let max = (2, 4, 5, 6, 20);
+max.Math.max();
